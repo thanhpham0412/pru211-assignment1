e161724422ff412eaa9581d8cdf53d27886b58f6
thanhpham0412 <ptt0412@gmail.com>
2023-01-12 12:32:39 +0700

    Init proj

---
+++ ProjectA012/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ProjectA012
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 1
+    /// </summary>
+    internal class Program
+    {
+        // number to classify
+        static int number;
+
+        /// <summary>
+        /// Programming Assignment 1
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // extract number from string
+                GetInputValueFromString(input);
+
+                // Add your code between this comment
+                // and the comment below. You can of
+                // course add more space between the
+                // comments as needed
+                string res = ((number % 2 == 0) ? "e" : "o") + " " + ((number == 0) ? "0" : ((number > 0) ? "1" : "-1"));
+                Console.WriteLine(res);
+
+                // Don't add or modify any code below
+                // this comment
+                input = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Extracts the number from the given input string
+        /// </summary>
+        /// <param name="input">input string</param>
+        static void GetInputValueFromString(string input)
+        {
+            number = int.Parse(input);
+        }
+    }
+}
---
+++ ProjectA013/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using LinkedLists;
+
+namespace ProjectA013
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 2
+    /// </summary>
+    internal class Program
+	{
+		// holds the list of input numbers
+		static UnsortedLinkedList<int> values = new UnsortedLinkedList<int>();
+
+		/// <summary>
+		/// Programming Assignment 2
+		/// </summary>
+		/// <param name="args">command-line args</param>
+		static void Main(string[] args)
+		{
+			// loop while there's more input
+			string input = Console.ReadLine();
+			while (input[0] != 'q')
+			{
+				// extract input values from string
+				BuildLinkedListFromString(input);
+
+				// Add your code between this comment
+				// and the comment below. You can of
+				// course add more space between the
+				// comments as needed
+				int times = 0, sum = 0;
+				while (values.Count > 0)
+                {
+					int num = values.Head.Value;
+					values.Remove(num);
+					if (num == -1)
+                    {
+						break;
+                    }
+					times++;
+					sum += num;
+                }
+				if (times == 0)
+                {
+					Console.WriteLine("0 0");
+
+				} else
+                {
+					Console.WriteLine(times + " " + (double)sum / (double)times);
+                }
+				
+
+
+				// Don't add or modify any code below
+				// this comment
+				input = Console.ReadLine();
+			}
+		}
+
+		/// <summary>
+		/// Builds a linked list of input values from provided string
+		/// </summary>
+		/// <param name="input">input string</param>
+		/// <returns>linked list of values</returns>
+		static void BuildLinkedListFromString(string input)
+		{
+			// find first space in string
+			int spaceIndex = input.IndexOf(' ');
+
+			// loop while there are more spaces in string
+			while (spaceIndex != -1)
+			{
+				// add value to list
+				int value = int.Parse(input.Substring(0, spaceIndex));
+				values.Add(value);
+
+				// move along string and find next space in string
+				input = input.Substring(spaceIndex + 1);
+				spaceIndex = input.IndexOf(' ');
+			}
+
+			// add final node to list
+			int finalValue = int.Parse(input);
+			values.Add(finalValue);
+		}
+
+		/// <summary>
+		/// Get a value from the linked list. If the linked
+		/// list is empty, returns -1
+		/// </summary>
+		/// <returns>value or -1 if list is empty</returns>
+		static int GetValue()
+		{
+			// check for empty list
+			int value;
+			if (values.Count > 0)
+			{
+				// save value at front of list
+				value = values.Head.Value;
+
+				// move along list
+				values.Remove(value);
+			}
+			else
+			{
+				// empty list
+				value = -1;
+			}
+			return value;
+		}
+	}
+}
